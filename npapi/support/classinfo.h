37ac7eae4ca46bd4d9bcd018742be5a6f2c810ef
Jean-Baptiste Kempf <jb@videolan.org>
2012-12-13 17:26:57 +0100

    Add license headers

--- npapi/support/classinfo.h
+++ npapi/support/classinfo.h
@@ -1,3 +1,31 @@
+/*****************************************************************************
+ * classinfo.h: helper class to implement all necessary nsIClassInfo methods
+ *****************************************************************************
+ * Copyright (C) 2002-2012 VideoLAN
+ * $Id$
+ *
+ * Authors: Samuel Hocevar <sam@zoy.org>
+ *          Damien Fouilleul <damienf.fouilleul@laposte.net>
+ *          Jean-Paul Saman <jpsaman@videolan.org>
+ *          Sergey Radionov <rsatom@gmail.com>
+ *          Jean-Baptiste Kempf <jb@videolan.org>
+ *          Cheng Sun <chengsun9@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include "nsIClassInfo.h"
 
 // helper class to implement all necessary nsIClassInfo method stubs
